6c2294c3bc572684ee2b9958d7fa107f9a99be56
Vishnu Krishnakumar <83391274+vishnukkrishna@users.noreply.github.com>
2024-01-09 12:56:16 +0530

    some change isn settings.py file

--- staffsense/settings.py
+++ staffsense/settings.py
@@ -71,10 +71,10 @@ ROOT_URLCONF = "staffsense.urls"
 
 TEMPLATES = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": ["templates"],
+        "BACKEND" : "django.template.backends.django.DjangoTemplates",
+        "DIRS"    : ["templates"],
         "APP_DIRS": True,
-        "OPTIONS": {
+        "OPTIONS" : {
             "context_processors": [
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
@@ -125,8 +125,8 @@ SIMPLE_JWT = {
 # Channels
 CHANNEL_LAYERS = {
     "default": {
-        "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {
+        "BACKEND"  : "channels_redis.core.RedisChannelLayer",
+        "CONFIG"   : {
             "hosts": [config("redis")],
         },
     },
@@ -154,15 +154,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE   = "en-us"
 
-TIME_ZONE = "UTC"
+TIME_ZONE       = "UTC"
 
-USE_I18N = True
+USE_I18N        = True
 
-USE_L10N = True
+USE_L10N        = True
 
-USE_TZ = True
+USE_TZ          = True
 
 AUTH_USER_MODEL = "authentication.Employee"
 
@@ -175,8 +175,9 @@ AUTHENTICATION_BACKENDS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "STATIC")]
+STATIC_URL       = "static/"
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
 
 
 MEDIA_ROOT = BASE_DIR / "media"
@@ -185,11 +186,11 @@ MEDIA_URL  = "/media/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD    = "django.db.models.BigAutoField"
 
 CORS_ORIGIN_ALLOW_ALL = True
 
-CORS_ALLOWED_ORIGINS = [
+CORS_ALLOWED_ORIGINS  = [
     "http://127.0.0.1:8000",
     "http://localhost:5173",
 ]
